3a97ea5ebc3e31a9fa55a17d22d499d2c9640e9e
TheCoolestFridge <jonathanfrigault1@gmail.com>
2019-06-14 19:54:41 -0400

    #29_DrawMultipleTargetZone:

--- Source/Ironside/PlayerCharacter.cpp
+++ Source/Ironside/PlayerCharacter.cpp
@@ -39,9 +39,6 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
 
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
-
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 }
