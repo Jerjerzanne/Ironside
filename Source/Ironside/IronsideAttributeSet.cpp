c21635e326ac698f335329a05520bb37a1ab0306
Simon Roy <simon.roy5219@gmail.com>
2019-07-04 14:52:15 -0400

    added some attributes stuff

--- Source/Ironside/IronsideAttributeSet.cpp
+++ Source/Ironside/IronsideAttributeSet.cpp
@@ -3,6 +3,7 @@
 
 #include "IronsideAttributeSet.h"
 #include "UnrealNetwork.h"
+#include "PlayerCharacter.h"
 
 
 UIronsideAttributeSet::UIronsideAttributeSet() 
@@ -16,6 +17,35 @@ void UIronsideAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
 	DOREPLIFETIME(UIronsideAttributeSet, Health);
+}
+
+void UIronsideAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+	FGameplayEffectContextHandle Context = Data.EffectSpec.GetContext();
+	UAbilitySystemComponent* Source = Context.GetOriginalInstigatorAbilitySystemComponent();
+	const FGameplayTagContainer& SourceTags = *Data.EffectSpec.CapturedSourceTags.GetAggregatedTags();
+
+	// Compute the delta between old and new, if it is available
+	float DeltaValue = 0;
+	if (Data.EvaluatedData.ModifierOp == EGameplayModOp::Type::Additive)
+	{
+		// If this was additive, store the raw delta value to be passed along later
+		DeltaValue = Data.EvaluatedData.Magnitude;
+	}
+
+	// Get the Target actor, which should be our owner
+	AActor* TargetActor = nullptr;
+	AController* TargetController = nullptr;
+	APlayerCharacter* TargetCharacter = nullptr;
+	if (Data.Target.AbilityActorInfo.IsValid() && Data.Target.AbilityActorInfo->AvatarActor.IsValid())
+	{
+		TargetActor = Data.Target.AbilityActorInfo->AvatarActor.Get();
+		TargetController = Data.Target.AbilityActorInfo->PlayerController.Get();
+		TargetCharacter = Cast<ARPGCharacterBase>(TargetActor);
+	}
+
 }
 
 void UIronsideAttributeSet::OnRep_Health()
